d17ca9d61f7ff740630702fa0e2d8661d6d8a1c0
Natanael Felix <f.natanael.felix.b@gmail.com>
2024-02-15 21:31:53 -0300

    feat(user): create, update & delete

--- src/main/java/com/springlearning/springmongo/services/UserService.java
+++ src/main/java/com/springlearning/springmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.springlearning.springmongo.services;
 
 import com.springlearning.springmongo.domain.User;
+import com.springlearning.springmongo.dto.UserDTO;
 import com.springlearning.springmongo.repositories.UserRepository;
 import com.springlearning.springmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,10 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
+    public static User fromDto(UserDTO dto) {
+        return new User(dto.getId(), dto.getName(), dto.getEmail());
+    }
+
     public List<User> findAll() {
         return repository.findAll();
     }
@@ -21,5 +26,27 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+    }
+
+    public User create(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User update(String id, User obj) {
+        Optional<User> opObj = repository.findById(id);
+
+        if (opObj.isEmpty())
+            throw new ObjectNotFoundException(id);
+
+        //update stuff-------------
+        opObj.get().setName(obj.getName());
+        opObj.get().setEmail(obj.getEmail());
+
+        return repository.save(opObj.get());
+    }
+
+    public void delete(String id) {
+        findById(id);
+        repository.deleteById(id);
     }
 }
